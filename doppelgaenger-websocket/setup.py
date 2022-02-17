453e23ed6bc601afc8b40d21c133c957c98b3a24
Jens Reimann <jreimann@redhat.com>
2022-02-17 18:34:30 +0100

    build: finally managed to containerize a python application

---
+++ doppelgaenger-websocket/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="drogue-doppelgaenger-websocket",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+)
+
