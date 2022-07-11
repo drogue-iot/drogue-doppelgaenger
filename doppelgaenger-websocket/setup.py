ca2b058c15e6041931d4db1c7203de05b38e4ec2
Jens Reimann <jreimann@redhat.com>
2022-07-11 12:12:11 +0200

    refactor: clean up old content

--- doppelgaenger-websocket/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="drogue-doppelgaenger-websocket",
-    version="0.1.0",
-    packages=find_packages(),
-    include_package_data=True,
-)
-
