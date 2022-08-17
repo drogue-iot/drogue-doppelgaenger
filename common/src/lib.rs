4ae4f394403748d389060e8b66363886b875307d
Jens Reimann <jreimann@redhat.com>
2022-08-17 11:07:51 +0200

    chore: clean up unused code

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,7 +1,5 @@
 use std::collections::HashMap;
 
-pub mod stream;
-
 #[derive(Clone, Debug, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
 pub struct KafkaConfig {
     pub properties: HashMap<String, String>,
